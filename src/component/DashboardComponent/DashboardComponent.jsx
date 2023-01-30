68ba608dbab6953162fec66c5bb22efdb822bafb
CreativeBrainStudio <charl.becina@gmail.com>
2023-01-30 13:44:40 +0800

    dashboard

--- src/component/DashboardComponent/DashboardComponent.jsx
+++ src/component/DashboardComponent/DashboardComponent.jsx
@@ -13,53 +13,51 @@ import backgroundImage from "../../BG.png";
 const DashboardComponent = ({ data = [] }) => {
   return (
     <ScrollComponent>
-    <Grid
-      container
-      direction={"column"}
-      sx={{
-        backgroundImage: `url(${backgroundImage})`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize:"cover",
-        minHeight:"100%",
-        
-        // height:"100%"
-       
-      }}
-      className="dashboard-component-container"
-    >
-      <HeaderComponent
-        headerLabel={"Dashboard"}
-        headerLabelIamges={dashboardIamge}
-      />
-
-
-<Grid
-        container
-        direction={"row"}
-        spacing={10}
-        padding={4}
-        justifyContent={"center"}
-        alignItems="center"
-      >
-        {data.map((each, index) => (
-          <Grid item key={index}>
-            <CardWithImage
-              imagePath={each.image}
-              totalNumber={each.number}
-              labelCard={each.label}
-            />
-          </Grid>
-        ))}
-      </Grid>
       <Grid
         container
-        justifyContent={"center"}
-        alignItems="center"
-        direction={"row"}
-        spacing={3}
-        padding={3}
+        direction={"column"}
+        sx={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          minHeight: "100%",
+
+          // height:"100%"
+        }}
+        className="dashboard-component-container"
       >
-        <Grid item xs={8} xl={6}>
+        <HeaderComponent
+          headerLabel={"Dashboard"}
+          headerLabelIamges={dashboardIamge}
+        />
+
+        <Grid
+          container
+          direction={"row"}
+          spacing={10}
+          padding={4}
+          justifyContent={"center"}
+          alignItems="center"
+        >
+          {data.map((each, index) => (
+            <Grid item key={index}>
+              <CardWithImage
+                imagePath={each.image}
+                totalNumber={each.number}
+                labelCard={each.label}
+              />
+            </Grid>
+          ))}
+        </Grid>
+        <Grid
+          container
+          justifyContent={"center"}
+          alignItems="center"
+          direction={"row"}
+          spacing={3}
+          padding={3}
+        >
+          {/* <Grid item xs={8} xl={6}>
           <Cardindicator
             darkTheme={false}
             setDetails={"Student pass for month for this september"}
@@ -73,13 +71,10 @@ const DashboardComponent = ({ data = [] }) => {
             setTitle={"Passer percentage"}
             chartEnable={true}
           />
+        </Grid> */}
         </Grid>
       </Grid>
-
-   
-    </Grid>
     </ScrollComponent>
-    
   );
 };
 
