c64911f4b4b24769bedc324101e3670961e81a71
CreativeBrainStudio <charl.becina@gmail.com>
2023-01-30 13:32:38 +0800

    topics

--- tesco_api-main/route/Topic_route.js
+++ tesco_api-main/route/Topic_route.js
@@ -1,11 +1,17 @@
-const express=require('express')
-const { create_topics, get_all_topics, delete_topics, update_topic } = require('../controller/Topic_controller')
-const Topics_Router=express.Router()
+const express = require("express");
+const {
+  create_topics,
+  get_all_topics,
+  delete_topics,
+  update_topic,
+  get_topics_by_user,
+} = require("../controller/Topic_controller");
+const Topics_Router = express.Router();
 
+Topics_Router.post("/create-topic", create_topics);
+Topics_Router.get("/topic", get_all_topics);
+Topics_Router.delete("/topic/:id", delete_topics);
+Topics_Router.put("/update-topic", update_topic);
+Topics_Router.get("/topics/:userId", get_topics_by_user);
 
-Topics_Router.post('/create-topic',create_topics)
-Topics_Router.get('/topic',get_all_topics)
-Topics_Router.delete('/topic/:id',delete_topics)
-Topics_Router.put('/update-topic',update_topic)
-
-module.exports=Topics_Router
+module.exports = Topics_Router;
