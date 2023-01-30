8437ca56302290d3ffb5ff8c4e8799a1d799f830
CreativeBrainStudio <charl.becina@gmail.com>
2023-01-30 13:12:03 +0800

    question_controller

--- tesco_api-main/route/Question_route.js
+++ tesco_api-main/route/Question_route.js
@@ -4,6 +4,7 @@ const {
   getall_question,
   delete_question,
   update_question,
+  get_filtered_questions,
 } = require("../controller/Question_controller");
 const Question_Router = express.Router();
 
@@ -11,5 +12,8 @@ Question_Router.post("/create-question", create_question);
 Question_Router.put("/update-question", update_question);
 Question_Router.get("/question", getall_question);
 Question_Router.delete("/question/:id", delete_question);
-Question_Router.get("/questions/:username/:topicName/:questionId");
+Question_Router.get(
+  "/questions/:username/:topicName/:questionId",
+  get_filtered_questions
+);
 module.exports = Question_Router;
